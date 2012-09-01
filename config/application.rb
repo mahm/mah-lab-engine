7c031279af1e50d5c0f6d0ad73e1d6a07107827c
mahm <nishimi@sonicgarden.jp>
2012-09-01 10:54:47 +0900

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module BlogMahLabCom
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Don't initialize on precompile
+    config.assets.initialize_on_precompile = false
   end
 end
